5e7d2184c392c7f295218f9707acfa7f5cb9896f
javoor1 <javoor1@hotmail.com>
2018-03-26 23:29:38 -0600

    https for fonts

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module GlasTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.paths << Rails.root.join("GlassMuebles", "assets", "images", "vendor", "javascripts", "js", "images")
+    # config.assets.paths << Rails.root.join("GlassMuebles", "assets", "images", "vendor", "javascripts", "js", "images")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
